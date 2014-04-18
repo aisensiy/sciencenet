2d31a48a6c6b21e82c63b7e6bae36d61ce649de4
aisensiy <aisensiy@gmail.com>
2014-04-18 14:42:47 +0800

    添加把 utf8 转成 gbk 的脚本

---
+++ iconv.sh
@@ -0,0 +1,4 @@
+iconv -f utf8 -t gbk < user.csv > user.gbk.csv
+iconv -f utf8 -t gbk < blog.csv > blog.gbk.csv
+iconv -f utf8 -t gbk < friend.csv > friend.gbk.csv
+iconv -f utf8 -t gbk < friendlink.csv > friendlink.gbk.csv
